4058d95a372f06420bee7bbc7e292447602b470e
Bro-Tech <k2785233247@gmail.com>
2024-08-18 16:57:13 +0800

    追加了对Vue工程化开发的初步理解，今天先到这xwx

--- Vue入门/7工程化开发和“脚手架”/formalvueproject-1/src/App.vue
+++ Vue入门/7工程化开发和“脚手架”/formalvueproject-1/src/App.vue
@@ -1,21 +1,44 @@
+<!-- 双标签template —— 结构区
+    可以理解成事这个Vue实例"自带的"一小段html
+    在Vue2中，template内只能有一个"根元素"(最外层只能有一个div)
+    但是Vue3就没有这样的限制了
+    (工程项目创建的为Vue2)
+-->
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img id="cirno" @click="shout" alt="Vue logo" src="./assets/cirno.gif"><br>
+    <strong id="baka">邪恶冰精，在这里宣布:</strong>
+    <HelloWorld msg="Welcome to Your First Baka Vue.js App"/>
+    <hr>
+    <button @click="welcome">你好，我是来自App.vue中的按钮owo</button>
   </div>
 </template>
 
+<!-- 逻辑(js)区 -->
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
-export default {
+// !! 也就是在这下面去写 data(在这里有点'特殊') methods computed watch 还有八个钩子函数 !!
+export default { //导出当前组件的配置项
   name: 'App',
   components: {
     HelloWorld
+  },
+  methods:{ //methods方法
+      shout(){
+        alert('baka');
+      },
+      welcome(){
+        alert("点我干嘛;w;")
+      }
+  },
+  created(){ //钩子函数
+    console.log("App.vue模版现在已经加载完成力!")
   }
 }
 </script>
 
+<!-- 样式区，对应设置的是上面template的css -->
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -24,5 +47,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#cirno{
+  width: 130px;
+  border: cyan dashed 10px;
+}
+#baka{
+  font-size:20px;
+  position: relative;
+  bottom: -10px;
 }
 </style>
--- Vue入门/7工程化开发和“脚手架”/formalvueproject-1/src/main.js
+++ Vue入门/7工程化开发和“脚手架”/formalvueproject-1/src/main.js
@@ -1,8 +1,23 @@
+// 这里就相当于 整个项目的"js核心"，这里的东西必定被执行
+console.log("HiHi~ main.js现在被执行力owo");
+
+//默认的写的作用: 先引入基础的vue开发包，导入App.vue，并利用App.vue里面写的"模版内容"去渲染指定的dom 
+//导基础vue开发包
 import Vue from 'vue'
+
+//导入 App.vue (根/默认自带 组件)
 import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false; //提示文本: F12控制台是否显示"开发环境"文本提示
 
+// Vue实例化，告知使用App.vue的"模版内容"去index.html中对指定dom(#app)进行渲染
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  //render: 选择某个"模版内容"，要记得return!
+  // render: h => h(App), 高级懒人简写xwx
+  
+  render: (anyNames) => { //这里的"anyNames" 是一个 "接收形参"，到时候会有个方法createElement()传给它
+    //然后在下面调用它,"()"里面放要引用的.vue文件名
+    return anyNames(App)
+  }
+
+}).$mount('#app') //和el:"xxx"功能一样，只是另外一种"挂载"写法XDD
