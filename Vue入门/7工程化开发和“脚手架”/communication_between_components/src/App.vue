94d02d4080dc3538b8c46379a6800fbebcc173d5
Bro-Tech <k2785233247@gmail.com>
2024-08-20 15:48:19 +0800

    又新建了一个脚手架，准备搞明白"组件通信"

---
+++ Vue入门/7工程化开发和“脚手架”/communication_between_components/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="App">
+    你好，这里是父组件App.vue
+  </div>
+</template>
+<script>
+
+
+export default {
+  
+}
+</script>
+<style scoped>
+  .App{
+    width: 600px;
+    height: 200px;
+    border: 4px solid black;
+    margin: 0px auto;
+    padding: 20px;
+    font-size: 24px;
+  }
+</style>
