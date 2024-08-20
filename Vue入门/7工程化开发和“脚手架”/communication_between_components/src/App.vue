f52524540e8df58abd4d0c233fe4ab57b6914c94
Bro-Tech <k2785233247@gmail.com>
2024-08-20 16:45:01 +0800

    完成了基本的各种数据类型的"父传子"，子组件'只简单展示'

--- Vue入门/7工程化开发和“脚手架”/communication_between_components/src/App.vue
+++ Vue入门/7工程化开发和“脚手架”/communication_between_components/src/App.vue
@@ -1,22 +1,36 @@
 <template>
   <div class="App">
     你好，这里是父组件App.vue
+    <!-- 在下面通过 ":子组件接收参数名='data中的数据名'" 给子组件传参-->
+    <ChildPart 
+    :pokemon="sylveon"
+    :lv="lv"
+    :skills="skills"
+    :ismale="male"
+    ></ChildPart>
   </div>
 </template>
 <script>
 
+import ChildPart from './components/ChildPart.vue';
 
 export default {
-  
+  components:{
+    ChildPart,
+  },
+  data(){
+    return{ //父组件要传递的数据可以是任意数据类型 (对象，数值，布尔，字符串...)
+      sylveon:{
+            name:"仙布",
+            species:"Fairy",
+        },
+      lv:47,
+      skills:["Bite","Draining Kiss","Tackle","Swift"],
+      male:false,
+    }
+  },
 }
 </script>
-<style scoped>
-  .App{
-    width: 600px;
-    height: 200px;
-    border: 4px solid black;
-    margin: 0px auto;
-    padding: 20px;
-    font-size: 24px;
-  }
+<style>
+
 </style>
---
+++ Vue入门/7工程化开发和“脚手架”/communication_between_components/src/components/ChildPart.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="App">
+        <p>Ciallo~,这里是子组件<br>接下来将展示从父组件中接收到的"妙妙信息~~"</p>
+        <hr>
+        <p>宝可梦名: {{ pokemon.name }}</p>
+        <p>类型: {{ pokemon.species }}</p>
+        <p>当前等级Lv: {{ lv }}</p>
+        <p>可用技能: {{ skills }}</p>
+        <p>性别 {{ ismale? "♂": "♀"}}</p>
+
+    </div>
+</template>
+<script>
+export default {
+  //
+  props:['pokemon','lv','skills','ismale']
+    
+}
+</script>
+<style scoped>
+  .App{
+    width: 600px;
+    height: 400px;
+    border: 4px solid black;
+    margin: 0px auto;
+    padding: 20px;
+    font-size: 24px;
+  }
+    
+</style>
