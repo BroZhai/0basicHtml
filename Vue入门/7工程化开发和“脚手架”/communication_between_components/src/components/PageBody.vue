640c7d155380a4ce513f09678d6b6ed10cadf9c9
Bro-Tech <k2785233247@gmail.com>
2024-08-21 11:18:52 +0800

    完成了基本的样式构建，准备去实现功能

--- Vue入门/7工程化开发和“脚手架”/communication_between_components/src/components/PageBody.vue
+++ Vue入门/7工程化开发和“脚手架”/communication_between_components/src/components/PageBody.vue
@@ -1,6 +1,9 @@
 <template>
-    <div>
-        
+    <div class="common">
+        <ul>
+            <!-- <li v-for="(item,index) in tasklist" :key="item.id">{{index+1}}. {{item.name}}&nbsp&nbsp&nbsp&nbsp
+                <button @click="del(item.id)">删除此项</button></li> -->
+        </ul>
     </div>
 </template>
 
@@ -11,5 +14,8 @@ export default {
 </script>
 
 <style>
-    
+    .common{
+        list-style: none;
+        text-align: center;
+    }
 </style>
--- Vue入门/7工程化开发和“脚手架”/communication_between_components/src/components/PageFooter.vue
+++ Vue入门/7工程化开发和“脚手架”/communication_between_components/src/components/PageFooter.vue
@@ -1,6 +1,7 @@
 <template>
-    <div>
-        
+    <div class="common">
+        <!-- <p>合计任务项:{{tasklist.length}} &nbsp&nbsp&nbsp&nbsp<button @click="clear">清除所有任务</button></p> -->
+        <em>样式不重要，重要的是功能XDD</em>
     </div>
 </template>
 
