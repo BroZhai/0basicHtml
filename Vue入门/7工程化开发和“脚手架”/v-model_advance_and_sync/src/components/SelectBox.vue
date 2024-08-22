134c1f8e107e49873d2e40a47427f8013a48bcbb
Bro-Tech <k2785233247@gmail.com>
2024-08-22 16:53:57 +0800

    实现了基本的 父组件"传默认选项值"给子组件

---
+++ Vue入门/7工程化开发和“脚手架”/v-model_advance_and_sync/src/components/SelectBox.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <select :value="selectedId">
+            <option value="S1">选项1</option>
+            <option value="S2">选项2</option>  
+            <option value="S3">父组件定的默认选中选项3</option>  
+            <option value="S4">选项4</option>  
+        </select>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['selectedId'],
+}
+</script>
+
+<style>
+    
+</style>
