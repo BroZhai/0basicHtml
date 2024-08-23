39766d776d49da9d6fa244c6788e9f03aa45ddd0
Bro-Tech <k2785233247@gmail.com>
2024-08-23 12:53:17 +0800

    简单实现了.sync的效果，一会来补下注释

--- Vue入门/7工程化开发和“脚手架”/v-model_advance_and_sync/src/App.vue
+++ Vue入门/7工程化开发和“脚手架”/v-model_advance_and_sync/src/App.vue
@@ -4,38 +4,60 @@
       :selectedId="selectedId"
 
       @plsChangeSid="okLetsChange"
+      
     ></SelectBox>
+    <!-- 注意，上面的@plsChangeSid后面也可以直接写成 行内命令 
+      @plsChangeSid="selectedId = $event" 
+      (这里的$event会获取$emit传过来的参数)
+    -->
     <br>
     <em>请在F12控制台查看相关输出信息</em>
+    <hr>
+    <div class="syncBox">
+      <h4>父组件当前synced的值为:</h4>
+      <b>{{ syncedValue }}</b>
+    </div>
+    <SendtoSyncedBox
+        :updateMe.sync="syncedValue"
+      ></SendtoSyncedBox>
   </div>
 </template>
 
 <script>
 
 import SelectBox from './components/SelectBox.vue';
+import SendtoSyncedBox from './components/SendtoSyncedBox.vue';
 
 export default {
 
   data(){
     return{
       selectedId:"S3",
+      syncedValue:"Default",
     }
   },
   
   components:{
     SelectBox,
+    SendtoSyncedBox,
   },
 
   methods:{
     okLetsChange(paraFromChild){
       this.selectedId=paraFromChild;
       console.log(`接收了到子组件的'plsChangeSid'请求，已修改当前的selectedId为"${this.selectedId}"`);
-    }
+    },
+
   }
 
 }
 </script>
-
-<style>
   
+<style>
+  .syncBox{
+    width: 300px;
+    height: 200px;
+    border: 3px solid black;
+    display: block;
+  }
 </style>
---
+++ Vue入门/7工程化开发和“脚手架”/v-model_advance_and_sync/src/components/SendtoSyncedBox.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h3>要Sync给父组件的消息</h3>
+        <input type="text" :value="updateMe" @change="passSync">
+    </div>
+</template>
+
+<script>
+export default {
+    props:["updateMe"],
+    methods:{
+        passSync($event){
+            let changedValue=$event.target.value;
+            this.$emit("update:updateMe",changedValue);
+        },
+    }
+}
+</script>
+
+<style>
+    input{
+        width: 20em;
+    }
+</style>
