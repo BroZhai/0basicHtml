670b2ab5d9b1c42ee137b3df9fb04f7a17e55ec7
Bro-Tech <k2785233247@gmail.com>
2024-08-24 14:45:19 +0800

    完成了$refs调用其他组件方法

--- Vue入门/7工程化开发和“脚手架”/ref-selectors_and_async-nexttick/src/App.vue
+++ Vue入门/7工程化开发和“脚手架”/ref-selectors_and_async-nexttick/src/App.vue
@@ -6,18 +6,41 @@
       <!--这是外部(最'上'层)的class名，是刻意用来误导querySelector的-->
     </div>
     <AnEchart></AnEchart>
+    <hr>
+    <!-- 现在，我们来试下 给子组件加ref, 看能不能过 -->
+    <LoginBox ref="logbox"></LoginBox>
+    <p>
+        <button class="btn" @click="loginBoxObj">获取登录信息</button> 
+        <button class="btn" @click="loginBoxClr">清除输入</button>
+    </p>
   </div>
 </template>
 
 <script>
 
 import AnEchart from './components/AnEchart.vue';
+import LoginBox from './components/LoginBox.vue';
 
 export default {
   components:{
     AnEchart,
-  }
+    LoginBox,
+  },
+
+  methods:{
+
+    loginBoxObj(){
+      this.$refs.logbox.getLoginObj();
+    },
+
+    loginBoxClr(){
+      this.$refs.logbox.clearLogin();
+    },
+
+  },
 }
+
+
 </script>
 
 <style>
---
+++ Vue入门/7工程化开发和“脚手架”/ref-selectors_and_async-nexttick/src/components/LoginBox.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="login">
+        <h3>LoginBox子组件登录框</h3>
+        <p>账户: &nbsp;&nbsp;<input type="text" placeholder="请输入账户名" v-model="account.username"></p>
+        <p>口令: &nbsp;&nbsp;<input type="password" placeholder="请输入口令" v-model="account.password"></p>
+        <!-- 注意，不是在这里写"登录"和"清除"按钮
+            去根组件中写，咱来试试$refs.子Vue组件 调用下面的methods方法
+        -->
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            // LoginBox子组件内部特有的'登录数据'
+            account:{
+                username:"",
+                password:""
+            },        
+        }
+    },
+    methods:{
+        getLoginObj(){
+            console.log(`子组件的getLoginObj()被执行了`)
+            console.log(`获取到的account信息为:
+        账号: ${this.account.username}
+        口令: ${this.account.password}
+            `);
+        },
+
+        clearLogin(){
+            this.account.username='',
+            this.account.password='',
+            console.log("子组件中的clearLogin()已被执行")
+        },
+    }
+}
+</script>
+<style>
+    .login{
+        width: 300px;
+        height: 160px;
+        border: 3px solid black;
+        border-radius: 15px;
+        text-align: center;
+    }
+    .login p{
+        margin-bottom: 20px;
+    }
+    .btn{
+        margin: 0px 32px;
+    }
+</style>
