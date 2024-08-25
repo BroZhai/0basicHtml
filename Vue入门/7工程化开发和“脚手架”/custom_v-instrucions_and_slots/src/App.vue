4e8f20c692f1f6d2f1d8eb1356633676bbb332d5
Bro-Tech <k2785233247@gmail.com>
2024-08-25 19:20:40 +0800

    全局注册了一个自定v指令 v-focus, 表示一进页面 直接选中输入框

--- Vue入门/7工程化开发和“脚手架”/custom_v-instrucions_and_slots/src/App.vue
+++ Vue入门/7工程化开发和“脚手架”/custom_v-instrucions_and_slots/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    
+    <!-- 我们先尝试去'全局注册'一个v-focus自动聚焦指令
+      一进页面立刻就"锁定输入框"
+    -->
+      <h2>自定义指令v-focus</h2>
+      <input type="text" style="width: 20em;" placeholder="我将会因v-focus而被进来时自动选中owo" v-focus>
   </div>
 </template>
 
--- Vue入门/7工程化开发和“脚手架”/custom_v-instrucions_and_slots/src/main.js
+++ Vue入门/7工程化开发和“脚手架”/custom_v-instrucions_and_slots/src/main.js
@@ -1,6 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 如果想"全局注册"一个所有组件通用的 自定v指令
+// 我们就需要在main.js中进行注册 
+/* 语法: Vue.directive('指令名',{
+          阶段(el,binding){
+            el: dom元素对象 的'接收形参'，内部可以直接这个形参调用其方法和属性
+            binding: 拿到v-自定义数据名=<u>数据</u> ，通过binding.value即可取得该 数据的值
+           (一些操作...)
+          },
+          ...
+          其他阶段...
+        })
+*/
+Vue.directive('focus',{
+  inserted(el){
+    el.focus(); //
+  },
+})
+
 Vue.config.productionTip = false
 
 new Vue({
