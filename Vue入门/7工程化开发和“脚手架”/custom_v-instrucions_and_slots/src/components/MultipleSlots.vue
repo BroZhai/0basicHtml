4558223d9be2d3f128d2fc6d551fdb614e62bb50
Bro-Tech <k2785233247@gmail.com>
2024-08-26 22:45:28 +0800

    追加了一些小注释，今天就先到这

--- Vue入门/7工程化开发和“脚手架”/custom_v-instrucions_and_slots/src/components/MultipleSlots.vue
+++ Vue入门/7工程化开发和“脚手架”/custom_v-instrucions_and_slots/src/components/MultipleSlots.vue
@@ -1,5 +1,8 @@
 <template>
     <div class="slot">
+        <!-- 在子组件中，用name="xxx"在slot后面给他们'起别名'
+            到时候在父组件中 在template后的v-slot:这里起的名字 进行选中
+        -->
         <h3><slot name="title">这个是默认Slot标题</slot></h3>
         <p><slot name="contentA">这是默认内容A</slot></p>
         <em><b><slot name="contentB">我是默认内容B</slot></b></em>
