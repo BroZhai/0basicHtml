0212220f64c8b7545e58a4044b137f754cf6e800
Working Tech <k2785233247@gmail.com>
2024-08-28 15:40:39 +0800

    单向输入框闪现后"自动聚焦"成功！

--- Vue入门/7工程化开发和“脚手架”/tag_customed_project/src/components/MyTag.vue
+++ Vue入门/7工程化开发和“脚手架”/tag_customed_project/src/components/MyTag.vue
@@ -1,6 +1,6 @@
 <template>
     <div v-if="isLocalDisplay">
-        <input type="text" placeholder="请输入要更改的备注">
+        <input type="text" placeholder="请输入要更改的备注" ref="noteBox">
     </div>
 
     <div v-else class="msgDisplay" @dblclick="clickChange">
@@ -36,8 +36,17 @@ export default {
     methods:{
         clickChange(){
             // this.$emit("clickChange",true);
-            this.isLocalDisplay=true; //用本地变量 控制每个单项的显示与否
-        }
+            //用本地变量 控制每个单项的显示与否
+            this.isLocalDisplay=true; 
+            
+            // 由于Vue的"异步性"，接下来的"自动聚焦 闪现输入框"需要用到$nextTick
+            this.$nextTick( ()=>{
+                // 异步之后 用$refs找到 闪现输入框的dom，调用其focus()即可 自动完成聚焦
+                this.$refs.noteBox.focus();
+            })
+
+        },
+
     }
 
 }
