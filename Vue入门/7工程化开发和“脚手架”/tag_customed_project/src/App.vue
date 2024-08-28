b3662404187f889782e117cce37a4729b193ad5d
Working Tech <k2785233247@gmail.com>
2024-08-28 13:57:03 +0800

    新建了一个工程，练习一下这段时间学到的东西，ref 和 slot

---
+++ Vue入门/7工程化开发和“脚手架”/tag_customed_project/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <MyTag></MyTag>
+  </div>
+</template>
+
+<script>
+
+import MyTag from './components/MyTag.vue';
+
+export default {
+  components:{
+    MyTag,
+  }
+}
+</script>
+
+<style>
+  
+</style>
