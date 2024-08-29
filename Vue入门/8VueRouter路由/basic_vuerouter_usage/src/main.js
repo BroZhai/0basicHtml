3e028a9d285fa55a4f7ad44c5de0d0404b4e27d1
Working Tech <k2785233247@gmail.com>
2024-08-29 15:37:17 +0800

    追加了简短的小注释

--- Vue入门/8VueRouter路由/basic_vuerouter_usage/src/main.js
+++ Vue入门/8VueRouter路由/basic_vuerouter_usage/src/main.js
@@ -1,13 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+
+// npm install好后引包
 import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
+// 用Vue的"官方安装".use()方式装官方包
 Vue.use(VueRouter)
 
+// 创建一个路由对象
 const router=new VueRouter({})
 
 new Vue({
   render: h => h(App),
-  router,
+  // 将创建的路由对象挂载到根组件上
+  router, //这是"router:router的简写"，如果路由对象为其他名字(r),这里要写成 router:r,
 }).$mount('#app')
