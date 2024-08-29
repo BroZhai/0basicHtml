e133e6f7d772aa4133f2b6820ab56f6b44518508
Working Tech <k2785233247@gmail.com>
2024-08-29 15:47:14 +0800

    完成了根目录基本的页面导航样式渲染

--- Vue入门/8VueRouter路由/basic_vuerouter_usage/src/App.vue
+++ Vue入门/8VueRouter路由/basic_vuerouter_usage/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    
+    <a class="box">消息</a>
+    <a class="box">联系人</a>
+    <a class="box">动态</a>
   </div>
 </template>
 <script>
@@ -11,5 +13,16 @@ export default {
 </script>
 
 <style>
-  
+  .box{
+    display: inline-block;
+    border: 2px solid black;
+    width:150px;
+    position: relative;
+    text-align: center;
+    margin-left: 100px;
+  }
+
+  .box:hover{
+    background-color: cyan;
+  }
 </style>
