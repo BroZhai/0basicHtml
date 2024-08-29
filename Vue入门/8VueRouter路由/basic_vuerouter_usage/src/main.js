d4b9567f1c424f3542ef4c1bc092751f24a631f7
Working Tech <k2785233247@gmail.com>
2024-08-29 15:33:57 +0800

    在main.js中完成了基本的VueRouter包引入

--- Vue入门/8VueRouter路由/basic_vuerouter_usage/src/main.js
+++ Vue入门/8VueRouter路由/basic_vuerouter_usage/src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const router=new VueRouter({})
 
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
