aa9436bdb9ee80d092281f9f2e14bc4c91de7c62
Working Tech <k2785233247@gmail.com>
2024-08-29 16:50:32 +0800

    小修小补

--- Vue入门/8VueRouter路由/basic_vuerouter_usage/src/App.vue
+++ Vue入门/8VueRouter路由/basic_vuerouter_usage/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-
       <div class="nav">
         <!-- 配置"自定义'路由'名"，一会对应main.js router对象中的 "path"项
           注意别忘了"挂载路由"后 多出来的"#"
@@ -10,9 +9,9 @@
         <a class="box" href="#/contacts">联系人</a>
         <a class="box" href="#/moments">动态</a>
         <h4>这里是一直保持不变的"导航栏组件"，变动的是下面展示的不同的"路由组件"</h4>
-        <em>注意上方的"路由地址后面也在变"</em>
+        <em>注意上方的"路由地址"后面也在变</em>
       </div>
-    
+    <!-- 别忘了要用<router-view> 对 "路由组件"的内容 进行展示！ -->
     <router-view></router-view>
   </div>
 
