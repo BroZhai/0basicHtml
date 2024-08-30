3e4effe1a1a00c65d941e94d715a6ea81fa78d23
Working Tech <k2785233247@gmail.com>
2024-08-30 16:05:55 +0800

    简单学习了页面重定向redirect 和 404处理 (本质就是捕获"全部的无效访问")

--- Vue入门/8VueRouter路由/advance_vue-router/src/router/index.js
+++ Vue入门/8VueRouter路由/advance_vue-router/src/router/index.js
@@ -4,6 +4,7 @@ import MomentPosts from "@/views/MomentPosts.vue";
 import SearchPage from "@/views/SearchPage.vue";
 import SearchResult from "@/views/SearchResult.vue";
 import RouteSearchResult from "@/views/RouteSearchResult.vue";
+import NotFound from "@/views/NotFound.vue";
 // Tips: "@"直接代表的就是 从"src"目录开始
 
 // 由于安装VueRouter要用到"大Vue"实例，路由模块默认没有，这里就要手动导入一下
@@ -24,6 +25,12 @@ const router=new VueRouter({
         // 这个是"设置诱捕器"的 动态路由传参 (用":"设置'诱捕名')
         {path:"/RoutePassingResultPage/:words?",component:RouteSearchResult},
         // Tips：最后面跟着的"?"表示 不传值时也可以访问该组件
+
+        // 追加一个"根页面"重定向redirect，自动跳转到"消息"路由组件
+        {path:"/",redirect:"/message"},
+
+        // 再来一个"404页面处理"，本质的原理就是"捕获全部*‘无效访问’"
+        {path:"*",component:NotFound},
     ],
 })
 // 配置完后，别忘了导出这个router对象
---
+++ Vue入门/8VueRouter路由/advance_vue-router/src/views/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="notfound">
+        <h3>哎呀，你访问的路径是个啥?</h3>
+        <img src="@/assets/fei.gif">
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+    .notfound{
+        text-align: center;
+        margin: 0px auto;
+    }
+    
+</style>
