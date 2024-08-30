3556c393d31450c9804ca949bfab255e3742dbe3
Working Tech <k2785233247@gmail.com>
2024-08-30 13:37:04 +0800

    搞明白的router-link及它带的两个妙妙class 来设置选中的"路由组件"样式

--- Vue入门/8VueRouter路由/advance_vue-router/src/App.vue
+++ Vue入门/8VueRouter路由/advance_vue-router/src/App.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
       <div class="nav">
+        <!-- 
         <a class="box" href="#/message">消息</a>
         <a class="box" href="#/contacts">联系人</a>
-        <a class="box" href="#/moments">动态</a>
+        <a class="box" href="#/moments">动态</a> 
+        -->
+        <router-link to="/message" class="box">消息</router-link>
+        <router-link to="/contacts" class="box">联系人</router-link>
+        <router-link to="/moments" class="box">动态</router-link>
         <h4>这里是一直保持不变的"导航栏组件"，变动的是下面展示的不同的"路由组件"</h4>
         <em>注意上方的"路由地址"后面也在变</em>
       </div>
@@ -37,5 +42,12 @@ export default {
 
   .box:hover{
     background-color: cyan;
+  }
+
+  /* 设置 选中"模糊"router-link 的样式 
+    设置其选中后背景变蓝
+  */
+  .router-link-active{
+    background-color: #0da0e9;
   }
 </style>
