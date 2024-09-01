4d25da01b6dfc8b7eb322a4884e7ac0b59f1fa90
Working Tech <k2785233247@gmail.com>
2024-09-01 15:52:55 +0800

    学会"自定义"新建了一个脚手架，准备来学习一下vuex (要开始注意代码书写格式规范了xwx, 排错排了半天)

---
+++ Vue入门/9Vuex全局多组件“数据共享”工具/lets-play_vuex/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h3>欢迎来到vuex教学！</h3>
+    <em>我草怎么这么多eslint报错? 看来要开始注意代码书写规范了OAO...</em>
+  </div>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style>
+</style>
