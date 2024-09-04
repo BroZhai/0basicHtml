2a22687160372b6b899271fc9346d8bd7bc95574
Working Tech <k2785233247@gmail.com>
2024-09-04 18:06:43 +0800

    整明白了...mapActions(['xxx'])辅助函数的用法，基本原理和前面的辅助函数都一样，记得要用前都要先导入！

--- Vue入门/9Vuex全局多组件“数据共享”工具/lets-play_vuex/src/components/SonComp1.vue
+++ Vue入门/9Vuex全局多组件“数据共享”工具/lets-play_vuex/src/components/SonComp1.vue
@@ -4,11 +4,14 @@
             <h4>Hi，这里是子组件1！owo</h4>
             <p>我从vuex中获取到的值为: {{ publicValue }}</p>
             <button @click="add">值 + 1</button>
+            &nbsp;
+            <button @click="delayChange(888)">延迟1s，设置值为888</button>
         </div>
     </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 export default {
   computed: {
     publicValue () {
@@ -22,7 +25,8 @@ export default {
       // 和父子组件通信一样，子组件不能直接修改父组件中的值，类似的，需要传一个"修改请求"
       // 用$store.commit('定义好的方法名') 去 访问 仓库mutations中写好的方法
       this.$store.commit('addinStore')
-    }
+    },
+    ...mapActions(['delayChange'])
   }
 }
 </script>
