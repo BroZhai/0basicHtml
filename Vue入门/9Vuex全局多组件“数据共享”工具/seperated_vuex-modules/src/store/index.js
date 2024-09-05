0b7c162c175bc60448cdce3f5216d730fb9e4304
Working Tech <k2785233247@gmail.com>
2024-09-05 17:04:43 +0800

    新建了一个脚手架，准备看看vuex的"模块拆分"modules:{}

---
+++ Vue入门/9Vuex全局多组件“数据共享”工具/seperated_vuex-modules/src/store/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  // vuex插件默认自己建好的东西
+  state: {
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
