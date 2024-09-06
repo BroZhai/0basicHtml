864b87dee3827788ba4e90c0f1a837e4213d2c80
Working Tech <k2785233247@gmail.com>
2024-09-06 16:11:12 +0800

    头一回自己纯纯的去把"商品"的样式框架给搓出来了，那么整体视图的样式就算是搞定了，今天先到这，明天继续研究具体的数据渲染和计算 (vuex全局数据の重头戏)

--- Vue入门/9Vuex全局多组件“数据共享”工具/vuex-exercise_project/src/components/CartItem.vue
+++ Vue入门/9Vuex全局多组件“数据共享”工具/vuex-exercise_project/src/components/CartItem.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    <p>这里是一个商品</p>
+  <div class="outer">
+    <div id="img">
+
+    </div>
+    <p id="itemName">这里是一个商品(名字)</p>
+    <p id="price">￥ xxx</p>
+    <p id="num">
+      <button> - </button> <b>商品数量</b> <button> + </button>
+    </p>
   </div>
 </template>
 
@@ -12,5 +19,38 @@ export default {
 </script>
 
 <style>
-  
+  .outer{
+    height: 110px;
+    border: 4px solid black;
+    margin-top: 5px;
+  }
+
+  #itemName{
+    margin-top: 2px;
+    font: normal bold 18px "黑体";
+  }
+
+  #img{
+    width: 100px;
+    height: 100px;
+    margin: 3px;
+    float: left;
+    position: relative;
+    display: block;
+    border: 2px solid grey;
+  }
+
+  #price{
+    margin-top: 38px;
+    position: relative;
+    float: left;
+    font: normal bold 18px "等线";
+    color: red;
+  }
+
+  #num{
+    position: relative;
+    float: right;
+    margin-right: 30px;
+  }
 </style>
