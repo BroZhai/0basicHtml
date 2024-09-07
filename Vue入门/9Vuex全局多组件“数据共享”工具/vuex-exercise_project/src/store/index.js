7e92e43db0f0f9c624139378664d488219a295c3
Working Tech <k2785233247@gmail.com>
2024-09-07 15:52:43 +0800

    建立了一个"购物车"小仓库module，并在大仓库中导入

--- Vue入门/9Vuex全局多组件“数据共享”工具/vuex-exercise_project/src/store/index.js
+++ Vue入门/9Vuex全局多组件“数据共享”工具/vuex-exercise_project/src/store/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+// 导入"购物车"小仓库module
+import cart from './modules/cart'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -13,5 +16,6 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
+    cart,
   }
 })
---
+++ Vue入门/9Vuex全局多组件“数据共享”工具/vuex-exercise_project/src/store/modules/cart.js
@@ -0,0 +1,21 @@
+// 构建 "购物车" 小仓库模块
+
+const state = { 
+  // 对象数组itemList，里面一个对象就是一个CartItem商品数据
+  itemList: [
+    {},
+  ],
+
+}
+const mutations = { }
+const actions = { }
+const getters = { }
+
+export default {
+  namespaced: true,
+
+  state,
+  mutations,
+  actions,
+  getters
+}
