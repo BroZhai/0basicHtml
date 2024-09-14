48b81db6ec173cdaab2ba9c86422a6131ab4f493
Working Tech <k2785233247@gmail.com>
2024-09-14 18:30:53 +0800

    使用vant-ui的"field输入框和"Button按钮"组件搭了一个简单的登录中心 (目前还是组件'全部导入')

--- Vue入门/奇怪好玩的“第三方库”/vant-ui/src/App.vue
+++ Vue入门/奇怪好玩的“第三方库”/vant-ui/src/App.vue
@@ -1,13 +1,63 @@
 <template>
   <div>
-    
+    <!-- OK，经典中的经典，我们来"重写扩张"一下HTML 9表单里面的东西
+        但这次是用别人"预制好"的组件，让整个页面更美观，且还有多出来的nb东西
+    -->
+        <div class="LoginBox">
+          <h3>劲爆登录Site</h3>
+          <!-- 下面就是vant-ui里面的"预制输入框"了awa  -->
+          <van-cell-group>
+            <van-field v-model="username" label="你的新劲爆id:" placeholder="请输入用户名" />
+            <van-field v-model="password" type="password" label="绕绕的口令: " placeholder="密码呢"/>
+
+          </van-cell-group>
+          <div class="btn">
+            <!-- 注意，按钮本身就已经"内置样式"了,
+              直接在van-button内再写class将不再生效，要想额外设置它，咱给它单独用个div套起来awa
+            -->
+            <van-button color="#7232dd" @click="login">提交信息</van-button>
+          </div>
+          <div class="btn">
+            <van-button color="grey" @click="resetInputs">重置</van-button>  
+          </div> 
+        </div>
+        <hr>
+        
   </div>
 </template>
 <script>
 export default {
-  
+  data () {
+    return{
+      username: "",
+      password: "",
+    }
+  },
+  methods:{
+    login(){
+      alert(`登录成功，获取到的用户名为: ${this.username}, 口令为: ${this.password}`)
+    },
+
+    resetInputs(){
+      this.username=''
+      this.password=''
+    }
+  }
 }
 </script>
 <style>
-  
+  .LoginBox {
+    border: 3px solid black;
+    width: 380px;
+  }
+  .LoginBox h3{
+    text-align: center;
+  }
+  .btn{
+    margin: 10px;
+    display: inline-block;
+    position: relative;
+    margin-left: 60px;
+  }
+
 </style>
--- Vue入门/奇怪好玩的“第三方库”/vant-ui/src/main.js
+++ Vue入门/奇怪好玩的“第三方库”/vant-ui/src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 我们现在这里尝试一下"直接导入全部组件"，按照官方的上手手册
+import Vant from 'vant';
+import 'vant/lib/index.css';
+
+Vue.use(Vant);
+
 Vue.config.productionTip = false
 
 new Vue({
