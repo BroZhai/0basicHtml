007994aba592fcdbbf4784859657bc38d934b4ef
Working Tech <k2785233247@gmail.com>
2024-09-19 19:12:55 +0800

    追加了App.vue根组件里面的所有注释，发现script和template换位置了，知道了"setup"对应着"组合式API"写法，了解了Vue3的一些其他特性: 模版允许多个"根组件" 和 组件"即导即用" (无需再手动注册了)

--- Vue入门/Vue3的大门/my-first-vue3/src/App.vue
+++ Vue入门/Vue3的大门/my-first-vue3/src/App.vue
@@ -1,22 +1,34 @@
+<!-- 哇哦? script和template换位置啦owo?
+    说是这样更符合逻辑
+-->
+
+<!-- 这里的"setup"关键字，就意味着用"组合式API"的方式去写 -->
 <script setup>
+// wow，Vue3这里导入组件之后就可以"直接在模版中用"了awa，不用再像以前一样额外写个components:{挨个注册...}
+// 即导即用
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
+  <!-- 在Vue3的template里面，我们发现根里面可以有多个"根元素"了
+      如下面的<header> 和 <main>，它俩就处于同一“根级别”下
+      而不像Vue2，template下只能有一个“大div” XDDDD
+    -->
+  <header> <!--第一个"根元素"-->
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="You did it!" /> <!--上面script导入的"HelloWorld组件"，无需再注册直接用-->
     </div>
   </header>
 
-  <main>
-    <TheWelcome />
+  <main>  <!--第二个"根元素"-->
+    <TheWelcome /> <!--另外导入的一个组件，同上-->
   </main>
 </template>
 
+<!-- 一如既往的CSS设置区，没什么太大的变化 -->
 <style scoped>
 header {
   line-height: 1.5;
