024f69ce76056a90ba336fff0f830d92f4e514c8
Working Tech <k2785233247@gmail.com>
2024-09-20 18:34:26 +0800

    在setup(){}里面写了一个数据+方法，并记得用return将他们手动传出去，给模版来使用

--- Vue入门/Vue3的大门/whatis-setup()/src/App.vue
+++ Vue入门/Vue3的大门/whatis-setup()/src/App.vue
@@ -1,7 +1,25 @@
 <script>
 export default {
-  setup(){
+  setup(){ //注意setup语法糖 不能和这个 setup(){}并存！
+    
     console.log(`哦吼，我比下面的beforeCreated哥们执行的更早耶~~`);
+    // 注意，在setup函数中，你是获取不到"this"的 (因为太太太太太早了XDDD)
+
+    // setup的本质作用: 给"数据块"(数据+对应函数)提供存放的地方
+    const mydata=919 //一个数据
+    const printMydata= ()=>{ //与这个数据相关的方法
+      console.log(`mydata的数据为: ${mydata}`);
+    }
+    
+    // 在一般情况下，在setup里面写的所有"数据"和"方法"都要'手动return'出去
+    return{
+      // 但是咱可以在<script>后面加个setup,之后这里面的setup(){}都可以不要了，直接写东西就好
+      mydata,
+      printMydata
+    }
+
+    // 我们现在来看一下setup语法糖 
+
   },
   beforeCreate(){
     console.log(`来自beforeCreated的消息: wc外挂！`);
@@ -12,10 +30,11 @@ export default {
 
 <template>
   <div>
-    
+    <button @click="printMydata">点我在打印mydata</button><br>
+    <p>{{ mydata }}</p>
+    <em>请在F12控制台中查看相应信息</em>
   </div>
 </template>
-
 
 
 <style>
