ed6721808119fb155d02c22db70713a6904e85ef
Working Tech <k2785233247@gmail.com>
2024-09-20 18:11:56 +0800

    初步研究了下setup()函数，原来它会比beforeCreated"更先于执行"(wc外挂！)

--- Vue入门/Vue3的大门/whatis-setup()/src/App.vue
+++ Vue入门/Vue3的大门/whatis-setup()/src/App.vue
@@ -1,8 +1,12 @@
 <script>
 export default {
   setup(){
-    
+    console.log(`哦吼，我比下面的beforeCreated哥们执行的更早耶~~`);
+  },
+  beforeCreate(){
+    console.log(`来自beforeCreated的消息: wc外挂！`);
   }
+  // 可以在控制台看到是setup()更先于执行
 }
 </script>
 
