f9a36b3bb22738ba44a016d67531f2691b6cacea
Working Tech <k2785233247@gmail.com>
2024-09-21 17:53:29 +0800

    把reactive()写了一下，就是简单的"死对象"转"或对象"

--- Vue入门/Vue3的大门/other-elementary-APIs/src/App.vue
+++ Vue入门/Vue3的大门/other-elementary-APIs/src/App.vue
@@ -1,47 +1,27 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive,ref } from 'vue';
+
+  // 先来演示纯"死对象"转"或对象"的reactive()
+  const testObj={
+    name:"Pancake",
+    lv:26,
+    skill:["近身搏斗",".45 ACP精准射击","M67范围空炸","EMP电子干扰"]
+  }
+  
+  let pureObj=reactive(testObj)
+  //在控制台打印reactive出来的 "活对象"pureObj
+  console.log(pureObj);
+
+
+
 </script>
 
-<template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+<template lang="">
+  <div>
+    {{ pureObj }}
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+<style>
+  
 </style>
