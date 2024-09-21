b0331de4350defe0ebc91dd24f62d88e47ad7869
Working Tech <k2785233247@gmail.com>
2024-09-21 18:41:02 +0800

    追加了组合式API的computed()函数理解，直接往()里面丢个回调函数+return就好了，这样可以让它和特定的'数据块'放到一起

--- Vue入门/Vue3的大门/other-elementary-APIs/src/App.vue
+++ Vue入门/Vue3的大门/other-elementary-APIs/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { reactive,ref } from 'vue';
+import { computed, reactive,ref } from 'vue';
 
   // 先来演示纯"死对象"转"或对象"的reactive()
   const testObj={
@@ -19,6 +19,7 @@ import { reactive,ref } from 'vue';
     lv:43,
     skill:["近身搏斗",".45 ACP精准射击","M67范围空炸","EMP电子干扰"]
   })
+  console.log(pure2.value.skill); //用ref()的话，对象也给存在'.value'里面了
 
   // 创建俩变动方法
   const dec = ()=>{
@@ -30,19 +31,36 @@ import { reactive,ref } from 'vue';
   }
   
 
+  // 接下来我们来看看组合式API的computed()函数
+  // 逻辑很简单，()里面直接写 '回调函数+return'就好啦 (对应特定的'数据块')
+  const numlist=ref([1,2,3,4,5,6,7,8,9,10]); //目标: 用computed 过滤出>4的所有数字
+  let filtered=computed( ()=>{
+    return numlist.value.filter( (curItem)=> curItem>=4 );
+  })
+
+
 </script>
 
 <template>
   <div>
+    <h3>reactive</h3>
     <p>pureObj: {{ pureObj }}</p>
     <!-- 下面放了个输入框，去尝试直接改"活对象"pureObj.name -->
     <input type='text' v-model="pureObj.name"></input>
+
     <hr>
+    <h3>ref()</h3>
     <p>simpleData当前的值为: {{ simpleData }}</p>
     <p>
       <button @click="dec"> -1 </button> <button @click="add"> +1 </button>
     </p>
     <p>pure2: {{ pure2 }}</p>
+    <input type='text' v-model="pure2.name"></input>
+    <hr>
+
+    <h3>组合式API的computed</h3>
+    <p>原数组: {{ numlist }}</p>
+    <p>组合式computed过滤后的: {{ filtered }}</p>
   </div>
 </template>
 
