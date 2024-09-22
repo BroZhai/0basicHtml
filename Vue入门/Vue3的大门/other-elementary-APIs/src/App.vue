deaba0cb3c49cb1c3db3c7b216e07709e355d60d
Working Tech <k2785233247@gmail.com>
2024-09-22 19:31:21 +0800

    重构了下文件夹，将不同的组合式API都拆成了单的的Vue模块，放在了components里面，根组件App.vue里面就只写了reactive()和ref()相关的东西 (觉醒了维护的意识?!!)

--- Vue入门/Vue3的大门/other-elementary-APIs/src/App.vue
+++ Vue入门/Vue3的大门/other-elementary-APIs/src/App.vue
@@ -1,6 +1,9 @@
 <script setup>
-import { computed, reactive,ref } from 'vue';
-
+import { reactive,ref } from 'vue';
+import Watch from './components/Watch.vue';
+import Computed from './components/Computed.vue';
+  // 根组件App.vue就用来演示reactive()和ref()了，剩下的其他组合式API就单独写成'对应的vue模块'放到components里面去了
+  // 不然这里的script将会是一大坨，极度的不美观((
 
   /* 先来演示纯"死对象"转"或对象"的reactive() */
   const testObj={
@@ -31,50 +34,15 @@ import { computed, reactive,ref } from 'vue';
     simpleData.value++;
   }
   
-
-  /* 接下来我们来看看组合式API的computed()函数 */
-  // 逻辑很简单，()里面直接写 '回调函数'就好啦 (对应特定的'数据块')
-  const numlist=ref([1,2,3,4,5,6,7,8,9,10]); //目标: 用computed 过滤出>4的所有数字
-  let filtered=computed( ()=>{
-    return numlist.value.filter( (curItem)=> curItem>=4 );
-  })
-
-  // 别忘了computed还有get set语法 (外部修改computed的'结果值')
-  let changedVal=ref(''); //注: 简单数据也要用".value"才能访问到它的内部数据 xwx
-  let letterList=ref(["T","E","K","O","N"]);
-    // 里面写个{}，里面再写get:... set:...
-  let filGetSet=computed({
-    // 保留"KON" (现用逻辑: 保留"E"下标之后的所有字母)
-    get: ()=>{
-     return letterList.value.filter( (curItem,index)=> {
-      if(index>1){
-        return curItem;
-      }
-     }) 
-    //  补充一下filter的用法 .filter( (当前遍历item,该item的index) => {执行的逻辑 + 是否return当前item或其他})
-    // 就不要记什么 奇怪的"简写"啦，务实的给他{条件+return}写全 xwx
-    },
-    set: (incomingValue) => {
-      letterList.value[letterList.value.length-1]=incomingValue
-      console.log(`已将letterList的最后一项变为: ${incomingValue}`)
-    }
-  })
-  const changeLast=() =>{
-    filGetSet.value=changedVal.value; //直接强行赋值computed的结果，触发上面的set
-  }
-
-
-  /* 接下来是watch()，就是用来监听数据变没变的那个哥们，分 新值 和 老值 */
-
 </script>
 
 <template>
   <div>
+    <!-- 演示reactive() 和 ref()创建响应式数据 -->
     <h3>reactive</h3>
     <p>pureObj: {{ pureObj }}</p>
     <!-- 下面放了个输入框，去尝试直接改"活对象"pureObj.name -->
     <input type='text' v-model="pureObj.name"></input>
-
     <hr>
     <h3>ref()</h3>
     <p>simpleData当前的值为: {{ simpleData }}</p>
@@ -84,14 +52,11 @@ import { computed, reactive,ref } from 'vue';
     <p>pure2: {{ pure2 }}</p>
     <input type='text' v-model="pure2.name"></input>
     <hr>
-
-    <h3>组合式API的computed</h3>
-    <p>原数组: {{ numlist }}</p>
-    <p>组合式computed过滤后的: {{ filtered }}</p>
-    <h4>computed{get: , set:}完整写法</h4>
-    <p>当前Computed到的内容: {{ filGetSet }}</p>
-    <input type='text' v-model="changedVal"></input>&nbsp;
-    <button @click="changeLast">修改最后一个值</button>
+    <!-- 接下来是 组合式computed -->
+    <Computed></Computed>
+    <hr>
+    <!-- 接下来是 组合式watch -->
+    <Watch></Watch>
     
   </div>
 </template>
---
+++ Vue入门/Vue3的大门/other-elementary-APIs/src/components/Computed.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h3>组合式API的computed</h3>
+    <p>原数组: {{ numlist }}</p>
+    <p>组合式computed过滤后的: {{ filtered }}</p>
+    <h4>computed{get: , set:}完整写法</h4>
+    <p>当前Computed到的内容: {{ filGetSet }}</p>
+    <input type='text' v-model="changedVal"></input>&nbsp;
+    <button @click="changeLast">修改最后一个值</button>
+  </div>
+</template>
+
+<script setup>
+  /* 接下来我们来看看组合式API的computed()函数 */
+  import { computed,ref } from 'vue';
+  // 逻辑很简单，()里面直接写 '回调函数'就好啦 (对应特定的'数据块')
+  const numlist=ref([1,2,3,4,5,6,7,8,9,10]); //目标: 用computed 过滤出>4的所有数字
+  let filtered=computed( ()=>{
+    return numlist.value.filter( (curItem)=> curItem>=4 );
+  })
+
+  // 别忘了computed还有get set语法 (外部修改computed的'结果值')
+  let changedVal=ref(''); //注: 简单数据也要用".value"才能访问到它的内部数据 xwx
+  let letterList=ref(["T","E","K","O","N"]);
+    // 里面写个{}，里面再写get:... set:...
+  let filGetSet=computed({
+    // 保留"KON" (现用逻辑: 保留"E"下标之后的所有字母)
+    get: ()=>{
+     return letterList.value.filter( (curItem,index)=> {
+      if(index>1){
+        return curItem;
+      }
+     }) 
+    //  补充一下filter的用法 .filter( (当前遍历item,该item的index) => {执行的逻辑 + 是否return当前item或其他})
+    // 就不要记什么 奇怪的"简写"啦，务实的给他{条件+return}写全 xwx
+    },
+    set: (incomingValue) => {
+      letterList.value[letterList.value.length-1]=incomingValue
+      console.log(`已将letterList的最后一项变为: ${incomingValue}`)
+    }
+  })
+  const changeLast=() =>{
+    filGetSet.value=changedVal.value; //直接强行赋值computed的结果，触发上面的set
+  }
+</script>
+
+<style>
+  
+</style>
