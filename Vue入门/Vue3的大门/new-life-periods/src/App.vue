0b2f593bd3d095b1041f1c78a52434f4dd225fc9
Working Tech <k2785233247@gmail.com>
2024-12-21 19:02:39 +0800

    新建了一个vue3项目，准备研究一下Vue3里面生命周期相比于vue2有了什么变化(奇了怪了哪来的自动样式，我记得之前没有的啊...)

---
+++ Vue入门/Vue3的大门/new-life-periods/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="title">
+      Vue3学习计划重启了
+    </div>
+    <div class="content">
+      <p>搞完之后去看看后端服务器之类的东西，好像蛮好玩的</p>
+      <em>2024/12/21</em>
+    </div>
+  </div>
+</template>
+
+
+<script setup>
+  import {ref} from 'vue'
+  const setupMsg = () => {
+    console.log('啊哈，setup里面的消息被触发了');
+  }
+  setupMsg();
+</script>
+
+
+<style>
+  .title{
+    text-align: center;
+    font-size: 40px;
+    font-style: normal;
+    color: red;
+
+  }
+</style>
