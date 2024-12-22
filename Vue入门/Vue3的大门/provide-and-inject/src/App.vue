6544e941357cf7e6353e1d61ba3f2012fde2be2a
Working Tech <k2785233247@gmail.com>
2024-12-22 19:41:46 +0800

    搞明白了一个比较抽象的问题，ref自身有一个value，但输入框也有一个value，所以用ref访问到输入框的值就是'两层.value' (ref名.value.value) XD

--- Vue入门/Vue3的大门/provide-and-inject/src/App.vue
+++ Vue入门/Vue3的大门/provide-and-inject/src/App.vue
@@ -2,8 +2,8 @@
   <div>
     <div id="outer">
       <h3>你好，我是父组件, 我只包含了Middle</h3>
-      <p><input type="text" placeholder="请输入要传的值"></p>
-      <p><button>传给最里面的组件</button></p>
+      <p><input type="text" placeholder="请输入要传的值" ref="inputValue"></p>
+      <p><button @click="sendProvide">传给最里面的组件</button></p>
       <MiddleComp></MiddleComp>
     </div>
   </div>
@@ -13,9 +13,19 @@
 import { provide, ref } from 'vue';
 import MiddleComp from './components/MiddleComp.vue';
 
+let inputValue = ref(null)
+
 // 准备provide一个值 给最里面的 Inner组件使用
 // 语法 provide('键名'，键值[变量])
-let bakaValue = ref(9)
+let bakaValue = ref(null)
+const sendProvide = () => {
+  //蚌埠住了，这里会有两个value (第一个value是ref要查值用的，第二个value是'输入框的值')
+  if(inputValue.value.value===""){
+    alert(`诶，你有输入什么东西吗awa?`)
+  }else{
+    bakaValue.value = ref(inputValue.value.value)
+  }
+}
 provide('receiveThis', bakaValue)
 </script>
 
