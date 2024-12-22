cffb9f0e8844ba15603c3eb54f91e3bf51f3581f
Working Tech <k2785233247@gmail.com>
2024-12-22 19:00:27 +0800

    新建了一个小工程，准备搞明白Vue3里面的provide和inject是怎么用的

---
+++ Vue入门/Vue3的大门/provide-and-inject/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div id="outer">
+      <h3>你好，我是父组件</h3>
+      <p><input type="text" placeholder="请输入要传的值"></p>
+      <p><button>传给最里面的组件</button></p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+  #outer{
+    text-align: center;
+    border: 3px solid black;
+  }
+</style>
