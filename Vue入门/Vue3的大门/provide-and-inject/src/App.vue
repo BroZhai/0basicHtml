85f170e4b1d3981cf710744ec11f9e62e3d4cef7
Working Tech <k2785233247@gmail.com>
2024-12-22 20:21:15 +0800

    成功实现项目逻辑，同时踩了一个大坑: 小心 ref() 套 ref()，再给变量赋值是尤其要小心! 今天先到这，下班~

--- Vue入门/Vue3的大门/provide-and-inject/src/App.vue
+++ Vue入门/Vue3的大门/provide-and-inject/src/App.vue
@@ -2,7 +2,9 @@
   <div>
     <div id="outer">
       <h3>你好，我是父组件, 我只包含了Middle</h3>
-      <p><input type="text" placeholder="请输入要传的值" ref="inputValue"></p>
+      <p>当前的值是: {{ bakaValue }}</p>
+      <p><input type="number" placeholder="请输入要传的值(仅数字)" 
+          ref="inputValue" style="height: 20px;" @keyup.enter="sendProvide"></p>
       <p><button @click="sendProvide">传给最里面的组件</button></p>
       <MiddleComp></MiddleComp>
     </div>
@@ -23,10 +25,29 @@ const sendProvide = () => {
   if(inputValue.value.value===""){
     alert(`诶，你有输入什么东西吗awa?`)
   }else{
-    bakaValue.value = ref(inputValue.value.value)
+    bakaValue.value = inputValue.value.value
+    console.log(`父组件已向'虚无空间'提供了值: ${inputValue.value.value}`);
   }
 }
-provide('receiveThis', bakaValue)
+
+
+provide('receiveThis', bakaValue) // '提供'输入框内的数据
+
+// 要想实现其他组件更改所provide的值，'丢数据的组件'也要把"修改方法"给丢进去
+// (修改方法对应的的还是'本组件'里面的修改，谁丢的数据谁负责)
+let addValue = () => {
+  bakaValue.value++;
+  console.log(`不知道哪个组件(其他组件)，请求的'添加方法+'已响应`);
+}
+
+let decValue = () => {
+  bakaValue.value--;
+  console.log(`不知道哪个组件(其他组件)，请求的'减小方法-'已响应`);
+}
+
+provide('addFunction', addValue)
+provide('decFunction', decValue)
+
 </script>
 
 <style>
--- Vue入门/Vue3的大门/provide-and-inject/src/components/InnerComp.vue
+++ Vue入门/Vue3的大门/provide-and-inject/src/components/InnerComp.vue
@@ -3,7 +3,8 @@
     <div id="inner">
       <h5>我是Inner</h5>
       <p>我当前收到的值为: {{ receivedValue }}</p>
-      <p><button>拿方法来+</button>&nbsp;&nbsp;&nbsp;<button>拿方法来-</button></p>
+      <p><button @click="AddFun">拿方法来+</button>&nbsp;&nbsp;&nbsp;
+        <button @click="DecFun">拿方法来-</button></p>
     </div>
     <em style="text-align: center;">请在F12控制台中查看对应日志</em>
   </div>
@@ -15,13 +16,11 @@ let receivedValue = ref(null)
 
 // 直接用inject('键名')取值，然后赋值到变量
 receivedValue = inject('receiveThis')
-const addReq = () =>{
-  
-}
 
-const decReq = () =>{
+// 拿到'虚无空间'里面两个'用于改值'的方法
+let AddFun = inject('addFunction')
+let DecFun = inject('decFunction')
 
-}
 </script>
 
 <style>
