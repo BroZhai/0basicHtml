af6ac8e6300ee8932f1452cdfa08e41475283e3c
Working Tech <k2785233247@gmail.com>
2024-12-25 19:51:53 +0800

    基本搞明白了defineOptions的用法

--- Vue入门/Vue3的大门/defineOptions-and-defineModel/src/App.vue
+++ Vue入门/Vue3的大门/defineOptions-and-defineModel/src/App.vue
@@ -1,47 +1,14 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h3>这里是暂时没有啥用的根组件</h3>
+    <!-- defineOptions的用法在/views的组件中有示例 -->
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script setup>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+</script>
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+<style>
+  
 </style>
---
+++ Vue入门/Vue3的大门/defineOptions-and-defineModel/src/views/view1.vue
@@ -0,0 +1,30 @@
+// 我们看这个文件名不难发现，这个文件名好像有点'过于简单了'
+<template>
+  <div>
+    <h4>假设我是一个'路由组件'，但是我的这个组件命名'并不符合规范' (大驼峰命名)</h4>
+    <p>并且在该组件中也没有'重新给组件规范命名'并导出，这就出问题了((</p>
+  </div>
+</template>
+
+<script setup>
+/* 在原本Vue2的写法中，我们可以直接在script中
+export{
+  name: "xxx"
+}
+来'规范命名'，但是现在这里是Vue3的setup语法，所以肯定是出了个'更简单'的方法
+不错，这正是我们'defineOptions'宏要干的
+*/
+
+//用前先导包，别忘了
+import {defineOptions} from 'vue'
+
+defineOptions({
+  name:"FormalNameForRouter1"
+  //...等其它属性
+})
+
+</script>
+
+<style>
+  
+</style>
