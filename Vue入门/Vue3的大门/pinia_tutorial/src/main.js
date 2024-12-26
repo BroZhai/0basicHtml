26803af944826fb477f2ea5b768ea97c74f349d2
Working Tech <k2785233247@gmail.com>
2024-12-26 12:08:19 +0800

    删掉了一些有的没的文件，同时为Pinia在main.s中的初始化加了点自己理解的注释

--- Vue入门/Vue3的大门/pinia_tutorial/src/main.js
+++ Vue入门/Vue3的大门/pinia_tutorial/src/main.js
@@ -1,11 +1,10 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createPinia } from 'pinia' // 导入Pinia'仓库创建器'
 import App from './App.vue'
 
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(createPinia()) // 给当前实例'正式安装'上Pinia插件 (虽是自动生成的，但也要了解)
 
 app.mount('#app')
