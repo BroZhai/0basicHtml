09799ae8c92edf525e6bb253565f20d926da8871
Working Tech <k2785233247@gmail.com>
2024-12-27 20:10:22 +0800

    了解了什么是storeToRefs()，这玩意对 解构仓库中'仅展示用'的数据特别方便，但是方法啥的解不了，简单的实践了下

--- Vue入门/Vue3的大门/pinia_tutorial/src/components/ZuJian1.vue
+++ Vue入门/Vue3的大门/pinia_tutorial/src/components/ZuJian1.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
-    <p>我是ZuJian1: 值为 {{ myStoreEntity.count }} <button @click="myStoreEntity.addCount">+</button></p>
+    <!-- 当我们在下面完成用 storeToRef()的解构赋值后，上面这里就可以直接用下面解出来的 '带响应式' 的值了awa -->
+    <p>我是ZuJian1: 值为 {{ count }} <button @click="myStoreEntity.addCount">+</button></p>
   </div>
 </template>
 
 <script setup>
 import { myStore } from '@/stores/counterStore';
+import { storeToRefs } from 'pinia';
+// 右侧先用 storeToRefs作用在'仓库方法'上，左侧申明你要抽出来的东西 (ES6)
+const { count } = storeToRefs(myStore())
+
+// 但是对于一般的方法来说，我们不能解构，还是按照'新建对象' = 调用函数() 的套路来
 const myStoreEntity = myStore()
 </script>
 
