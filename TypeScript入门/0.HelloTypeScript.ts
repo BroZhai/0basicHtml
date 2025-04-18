9fd13a25b25fb0ad9b87ce04258879df18cee49a
Working Tech <k2785233247@gmail.com>
2025-04-18 17:39:49 +0800

    追加了ts一点很新的数据类型

--- TypeScript入门/0.HelloTypeScript.ts
+++ TypeScript入门/0.HelloTypeScript.ts
@@ -1,7 +1,21 @@
 // js的原始数据类型
-let integer: number = 114514
+let integer: number = 114514 
 let character: string = '我是字符串'
 let tf: boolean = false
-let unknown: undefined // 你不许给我赋值(恼
-let lonely: null = null // 我是虚无的空白
+let unknown: undefined = undefined 
+let lonely: null = null 
+function testFun(): void{ 
+  console.log('这是一个没有返回值的方法, 已声明返回值为void');
+}
 let es6WoW: symbol = Symbol('我的地址是独一无二的!')
+
+// ts给你整出的点'很新的东西'
+
+  /* 联合类型 */
+  let mix_arr: (string|number)[] = ['我既可以字符串','又可以是', 114514]
+  let sepeate_judge: string[] | number = 1234 // 注意这个byd'()', 这里的意思是: 要么是'字符串数组[]', 要么是'纯数字'
+
+  /* 自定义类型 (类型别名)*/
+  type MyAny = number | boolean 
+  // let myData: MyAny = 'ss' // 错误示范, 数据类型错误
+  let myData: MyAny = true // 对应到MyAny中的 boolean, Correct!
