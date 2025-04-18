5c89ff0b83997f5226f5e9df81571c5c274bad02
Working Tech <k2785233247@gmail.com>
2025-04-18 16:57:31 +0800

    追加了劲爆TypeScript, 原来是给JavaScript的'数据类型'擦屁股 XD

---
+++ TypeScript入门/0.HelloTypeScript.ts
@@ -0,0 +1,7 @@
+// js的原始数据类型
+let integer: number = 114514
+let character: string = '我是字符串'
+let tf: boolean = false
+let unknown: undefined // 你不许给我赋值(恼
+let lonely: null = null // 我是虚无的空白
+let es6WoW: symbol = Symbol('我的地址是独一无二的!')
