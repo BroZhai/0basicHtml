1c47d84a19e1dd0444bebc58ba0fc32292397768
Working Tech <k2785233247@gmail.com>
2025-04-19 19:50:25 +0800

    详细了解了类型推断 和 类型断言

---
+++ TypeScript入门/1.TypeAssert.ts
@@ -0,0 +1,8 @@
+// 我们来具体看一下ts中的'类型推断' 和 '类型断言'
+
+/* 类型推断 */
+let age = 18; // 未标明number, 但鼠标浮在age上会发现有'number'
+
+/*类型断言*/
+const aLabel = document.getElementById('link') // 拿到id为link的 a标签dom, 但属性为'通用HTML Elements'
+const aLable = document.getElementById('link') as HTMLAnchorElement // as 'a标签'
