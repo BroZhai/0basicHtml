99a90bd9ad5b181423deb12ed117ab2c6cb39574
Working Tech <k2785233247@gmail.com>
2025-04-21 19:50:23 +0800

    为'类型断言'追加了一些额外的内容

--- TypeScript入门/1.TypeAssert.ts
+++ TypeScript入门/1.TypeAssert.ts
@@ -4,5 +4,11 @@
 let age = 18; // 未标明number, 但鼠标浮在age上会发现有'number'
 
 /*类型断言*/
-const aLabel = document.getElementById('link') // 拿到id为link的 a标签dom, 但属性为'通用HTML Elements'
-const aLable = document.getElementById('link') as HTMLAnchorElement // as 'a标签'
+// const aLabel = document.getElementById('link') // 拿到id为link的 a标签dom, 但属性为'通用HTML Elements'
+// const aLable = document.getElementById('link') as HTMLAnchorElement // as 'a标签'
+
+/*额外补充*/
+let obj = {a:100, b:200}
+let key = 'a' as keyof typeof obj // key 现在即为 'a' | 'b'
+console.log(typeof obj);
+console.log(obj[key]);
