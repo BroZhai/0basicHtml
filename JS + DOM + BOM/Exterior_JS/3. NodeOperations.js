772962923d3b6d5eaadc8208618bff44ae94d1d1
Working Tech <k2785233247@gmail.com>
2025-08-28 19:08:21 +0800

    实践了怎么创建'新节点'并插入到页面指定的节点中, 整理了相关的笔记

--- JS + DOM + BOM/Exterior_JS/3. NodeOperations.js
+++ JS + DOM + BOM/Exterior_JS/3. NodeOperations.js
@@ -8,3 +8,11 @@ console.log(`${p_parent.id}中的子集元素节点有: `);
 console.dir(children_nodelist);
 console.log(`第一个子集元素的innerHtml为: ${children_nodelist[0].innerHTML}`);
 console.log(`最后一个为: ${children_nodelist[children_nodelist.length-1].innerHTML}`);
+
+let insert_p = document.createElement("p"); // 使用document.createElement创建一个p标签
+insert_p.innerHTML = "我是来自JS定义的p标签";
+// 直接插在 inner_div末尾
+// p_parent.appendChild(insert_p); 
+
+// 在inner_div指定'参考元素'位置前插入, 使用insertBefore(要插入的子元素节点, 参考元素节点)方法;
+p_parent.insertBefore(insert_p, children_nodelist[1]); // 插在第一个p标签(qwq)前
