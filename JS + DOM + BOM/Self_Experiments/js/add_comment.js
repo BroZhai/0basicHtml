ddaffeacca797a6d2985b6fa43f3389c2679bf9c
Working Tech <k2785233247@gmail.com>
2025-08-29 14:24:06 +0800

    简单实践了'按钮删评论'功能, 了解了节点的'删除'操作

--- JS + DOM + BOM/Self_Experiments/js/add_comment.js
+++ JS + DOM + BOM/Self_Experiments/js/add_comment.js
@@ -10,7 +10,7 @@ let index_counter = 1;
 edit_elements[1].onclick = () => {
     const input_comment = edit_elements[0].value;
     let new_comment = document.createElement("li");
-    new_comment.innerHTML = input_comment;
+    new_comment.innerHTML = input_comment + "&nbsp;&nbsp;<button>删除</button>";
     index_counter++;
     new_comment.setAttribute("data-index", index_counter);
     if(index_counter%2==0){
@@ -20,4 +20,17 @@ edit_elements[1].onclick = () => {
 
     display_ul.appendChild(new_comment);
     edit_elements[0].value=''; // 清空已有描述
+
+    // 删评论按钮功能实现    
+    let del_btn = new_comment.children[0];
+    del_btn.onclick = ()=>{
+        console.log(`评论: "${new_comment.innerHTML}" 已被移除`);
+        display_ul.removeChild(new_comment);
+    }
+    
+}
+
+let first_delete_btn = display_elements[0].children[0]; // 默认第一条评论的删除按钮
+first_delete_btn.onclick = () =>{
+    alert("第一个评论被我保护了, 不能删哦qwq")
 }
