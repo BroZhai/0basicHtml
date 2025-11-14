8e583cb386bd3a76bab123ac208a084556357a6f
Working Tech <k2785233247@gmail.com>
2025-11-14 15:37:44 +0800

    简单完成了内置path库的实验

--- Node.js入门/0.built_in_modules/path.cjs
+++ Node.js入门/0.built_in_modules/path.cjs
@@ -1,2 +1,15 @@
 const path = require("path");
-let
+const fs = require("fs");
+
+const append_dir = '/file_dir/a.txt';
+console.log(`当前文件的__dirname常量为: ${__dirname}`);
+let joined_path = path.join(__dirname, append_dir);
+console.log(`和append_dir拼接得到的结果为: ${joined_path}`);
+
+fs.readFile(joined_path, (err,data) =>{
+    if(err){
+        console.log("文件不存在!");
+    }else{
+        console.log("File Hit!");
+    }
+})
