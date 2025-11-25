61fcbf1e94a9acd8c6d13776a5508e0412896cee
Working Tech <k2785233247@gmail.com>
2025-11-25 18:42:00 +0800

    尝试了用express的内置中间件捕获json请求体数据, 补充了之前端'表单提交'的知识盲区, 知道了如何更改原来的要提交的'表单数据'urlencode 为json, 同时被js的for of 还有 for ... in 坑了一把, 还有该死的CORS是个什么东西, 找时间研究明白

--- Node.js入门/2.third_party_modules/middle_ware.js
+++ Node.js入门/2.third_party_modules/middle_ware.js
@@ -31,9 +31,22 @@ const mid_fun_4 = (req, res, next) => {
 }
 
 
+// 简单CORS处理，允许从file://或其他来源访问
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(204);
+    }
+    next();
+});
+
 // 注册为'全局中间件' (每当有请求过来时, 按照'.use()'的顺序依次调用)
 app.use(mid_fun_1);
 app.use(mid_fun_2);
+
+app.use(express.json()); // 配置 '解析json表单数据'的中间件, 随后会在请求体req的 body属性中生成对应的解析结果
 
 // 路由匹配部分
 app.get("/glo_mid", (req, res) => {
@@ -44,4 +57,17 @@ app.get("/glo_mid", (req, res) => {
 app.get("/loc_mid", [mid_fun_3, mid_fun_4], (req, res) =>{
     console.log(`路由: 从req中得到的cus_str的值为: ${req.cus_str}`);
     res.send("看看后端控制台有没有'局部组件'运行的打印信息?")
+})
+
+// express.json 处理json表单数据 (监听指定的api提交地址, 随后解析请求体数据)
+app.post("/api/submit", (req, res) => {
+    console.log("api submit post请求被触发!");
+    if(req.body){
+        console.log(req.body);
+    }else{
+        console.log(`req.body的内容为空!`);
+    }
+    res.json({status:"ok"});
+    // res.send("感谢发送");
+    
 })
