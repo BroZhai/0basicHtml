a9817adbdbb63d4ba0eaf5162cc8fda036cdcc01
Working Tech <k2785233247@gmail.com>
2025-11-26 16:20:16 +0800

    简单的编写一个一个get接口和一个post接口

--- Node.js入门/2.third_party_modules/api_handle/router.js
+++ Node.js入门/2.third_party_modules/api_handle/router.js
@@ -1,8 +1,27 @@
 const express = require('express')
-const router = require(express.Router());
+const router = express.Router();
 
-router.get('/url', (req, res) => {
-
+// 一个简单的get请求处理接口
+router.get('/get', (req, res) => {
+    const query = req.query; // 从客户端拿'get查询语句' (?key1=value1&key2=value2...)
+    console.log(query);
+    // 省略与后台SQL服务器交互的代码
+    res.send({ // 发送回应给浏览器
+        handle_status: 200,
+        msg: "自定义的msg参数, 你触发了get请求的处理API",
+        data: query
+    })
 });
+
+// 写post请求接口也是完全同理 (处理post请求时, 需要提前配置[注册]'解析请求体'的中间件,)
+router.post('/post', (req, res) => {
+    const query = req.query;
+    // 省略与后台SQL服务器交互的代码
+    res.send({
+        handle_status: 201,
+        msg: "你成功出发了post请求的处理API",
+        data: query
+    })
+})
 
 module.exports = router; // 导出该路由
--- Node.js入门/2.third_party_modules/api_handle/server.js
+++ Node.js入门/2.third_party_modules/api_handle/server.js
@@ -1,5 +1,13 @@
 const express = require("express");
-const my_router = require("./router");
+const my_router = require("./router.js");
 const app = express();
+
+// 运行服务器
+app.listen(8080, () => {
+    console.log("Server is now running at http://locathost:8080");
+})
+
+// 配置'解析表单数据'的中间件 (用内置的URL-encoded, 处理post请求时就会触发)
+app.use(express.urlencoded({extended: false}));
 
 app.use('/api', my_router); // 注册'自定义路由'中间件 (加个'指定api前缀'才会触发)
