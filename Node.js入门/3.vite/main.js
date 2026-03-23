b2f917d5c08ba032e2cfd4afdfe5046eb8dab500
Working Tech <k2785233247@gmail.com>
2026-03-23 15:32:05 +0800

    玩转了下SurveyJS库

--- Node.js入门/3.vite/main.js
+++ Node.js入门/3.vite/main.js
@@ -3,6 +3,9 @@ import axios from "axios";
 // import axios from "./node_modules/axios/lib/axios.js"
 import {skills} from "./test.json"
 
+import { Model } from "survey-core";
+import * as surveyUI from "survey-js-ui";
+
 
 // console.log(`从counter.js导入的值: ${counter}`);
 // console.log(axios);
@@ -52,3 +55,77 @@ scan_folder.addEventListener("click", async (e) => {
         folder_list_ele.innerHTML += `<li><a href="/check_folder?folder=${dirname}">${dirname}</a></li>`;
     }
 })
+
+const json1 = {
+  elements: [{
+    type: "checkbox",
+    name: "programmingLanguages",
+    title: "What programming languages do I use?",
+    choices: [
+      { value: "c++", text: "C++"},
+      { value: "js", text: "JavaScript" },
+      { value: "java", text: "Java" }
+    ],
+    correctAnswer: "java",
+    score: 20
+  }, {
+    type: "radiogroup",
+    name: "AAA",
+    title: "Who is baka?",
+    choices: [
+      { value: "IceWing", text: "IceWing"},
+      { value: "Tekon", text: "Tekon" },
+      { value: "Cirno", text: "Cirno" }
+    ],
+    correctAnswer: "IceWing",
+    score: 30
+  }]
+};
+
+const json2 = {
+  elements: [{
+    type: "radiogroup",
+    name: "AAA",
+    title: "What programming languages do you use?",
+    choices: [
+      { value: "BBB", text: "BBB"},
+      { value: "js", text: "CCC" },
+      { value: "java", text: "DDD" }
+    ],
+    // showOtherItem: true
+  }]
+};
+
+
+let question = new Model(json1);
+console.log(question.validate());
+
+// question.data = "Baka";
+// question.onCompleting.add( (sender) =>{
+//   let total_score = 0;
+//   let max_score = 0;
+
+//   sender.getAllQuestions().forEach((question) => {
+//     if (question.score !== undefined) {
+//       max_score += question.score;
+//     }
+//     if (question.isAnswerCorrect()) {
+//       total_score += Number(question.score);
+//     }
+//   });
+
+//   sender.setVariable("total_score", total_score);
+//   sender.setVariable("max_score", max_score);
+//   console.log(`总分: ${total_score}, 得分: ${max_score}`);
+// })
+
+document.addEventListener("DOMContentLoaded", () => {
+    question.render(document.getElementById("surveyContainer"));
+});
+
+let refresh_btn = document.querySelector("#refresh");
+refresh_btn.addEventListener("click", () => {
+    question = new Model(json2);
+    question.render(document.getElementById("surveyContainer"));
+    // question.render(document.getElementById("surveyContainer"));
+});
